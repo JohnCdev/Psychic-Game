412a211dc48cf53e05dcc48a8c54b7ebc0ad6155
JohnCdev <johncarverp@gmail.com>
2019-07-29 22:23:31 -0400

    added full array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,9 @@ var losses = document.getElementById("losses");
 var left = document.getElementById("left");
 var guesses = document.getElementById("guesses");
 
-var computerChoices = ["q","w","e","r","t","y","u","i","o","p"];
+var computerChoices = [ "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", 
+                        "a", "s", "d", "f", "g", "h", "j", "k", "l",
+                        "z", "x", "c", "v", "b", "n", "m"];
 
 var winsCount = 0;
 var lossesCount = 0;
@@ -11,16 +13,11 @@ var leftCount = 10;
 
 left.innerHTML = " " + leftCount;
 
-// wins.innerHTML = (" " + "1");
-// losses.innerHTML = (" " + "2");
-// left.innerHTML = (" " + "3");
-// guesses.innerHTML = (" " + "4");
-
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
 document.onkeyup = function (event) {
 
-    var userGuess = event.key;
+    var userGuess = event.key.toLowerCase();
 
     guesses.append(" " + userGuess + ",");
 
